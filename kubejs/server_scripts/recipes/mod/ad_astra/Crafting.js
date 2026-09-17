fac018d9e96e54c63078fd7ef0ef48754a6dccbb
逐日炎雪中微子 <eternalsnowstorm@foxmail.com>
2026-09-17 14:21:41 +0800

    Revert "Core"
    This reverts commit eb541c78f944a3a742f5167783165ff7809a6958.

--- kubejs/server_scripts/recipes/mod/ad_astra/Crafting.js
+++ kubejs/server_scripts/recipes/mod/ad_astra/Crafting.js
@@ -10,4 +10,16 @@ ServerEvents.recipes((event) => {
 		A: "#forge:rods/iron",
 		B: "#forge:plates/iron"
 	}).id("ad_astra:vent")
+
+	// 燃煤发电机
+	kubejs.shaped("ad_astra:coal_generator", [
+		"ABA",
+		"ACA",
+		"DDD"
+	], {
+		A: ["#forge:ingots/iron", "#forge:plates/iron"],
+		B: Mechanisms.IRON.COM,
+		C: ["minecraft:furnace", "minecraft:blast_furnace", "minecraft:smoker"],
+		D: "#forge:plates/industrial_iron",
+	}).id("ad_astra:coal_generator")
 })
